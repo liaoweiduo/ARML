8a0a2d41a3861e512fad537f89d3572b16463e6b
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2022-03-05 21:25:47 +0800

    Update BA_1shot.sh

--- plainmulti_bash/BA_1shot.sh
+++ plainmulti_bash/BA_1shot.sh
@@ -10,8 +10,23 @@ export CUDA_VISIBLE_DEVICES=$GPU_ID
 
 #python main.py --datasource=BA --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01
 
+# Bird
+#python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=0 --train=False --test_epoch=49000
+
+# Aircraft
+#python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=2 --train=False --test_epoch=49000
+
+# Texture
+#python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=1 --train=False --test_epoch=49000
+
 # Fungi
-python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=3 --train=False --test_epoch=49000
+#python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=3 --train=False --test_epoch=49000
+
+# mini-imagenet
+#python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=3 --train=False --test_epoch=49000
+
+# Flower
+python main.py --datasource=VGG_Flower --datadir=/liaoweiduo/datasets --metatrain_iterations=50000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=/liaoweiduo/ARML/BA --num_filters=32 --hidden_dim=128 --emb_loss_weight=0.01 --test_dataset=0 --train=False --test_epoch=49000
 
 
 # Aircraft results:
@@ -27,6 +42,13 @@ python main.py --datasource=plainmulti --datadir=/liaoweiduo/datasets --metatrai
 #  0.31439954, 0.3145995 , 0.3151995 , 0.3155995 , 0.3157995 ,
 #  0.3155995 , 0.31619948, 0.31639948, 0.3157995 , 0.3153995 ,
 #  0.31499952]
+
+# Fungi results:
+# [0.19719902, 0.33279973, 0.33259976, 0.33279976, 0.33259982,
+#  0.33259982, 0.33279982, 0.33279982, 0.3327998 , 0.3327998 ,
+#  0.3321998 , 0.3327998 , 0.33279982, 0.3327998 , 0.3323998 ,
+#  0.33219978, 0.33259976, 0.33259976, 0.33219975, 0.33199975,
+#  0.33199975]
 
 # mini-imagenet results:
 # [0.19959894, 0.34339982, 0.34439993, 0.34599987, 0.34619987,
