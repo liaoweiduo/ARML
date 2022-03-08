b69e0bb81f7f58f5671d444a9de90bf8f7ef7daa
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2022-03-08 23:19:20 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -8,7 +8,7 @@ import sys
 with open('/liaoweiduo/ARML/BA/all_filenames.json', 'r') as f:
     all_filenames = json.load(f)
 
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 # make queue for tensorflow to read from
 filename_queue = tf.train.string_input_producer(tf.convert_to_tensor(all_filenames), shuffle=False)
@@ -20,4 +20,7 @@ image_name, image_file = image_reader.read(filename_queue)
 tf.print('file: ', image_name, output_stream=sys.stderr)
 image = tf.image.decode_jpeg(image_file, channels=3)
 
-
+sess = tf.Session()
+with sess.as_default():
+    sess.run(image)
+    
